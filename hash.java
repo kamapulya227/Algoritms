e27534fdec79e9d9a5c8850f2bf0d71399889ea7
kamapulya227 <kim.ase4ka@yandex.ru>
2025-10-02 15:36:32 +0300

    Update hash.java

--- hash.java
+++ hash.java
@@ -1,23 +1,15 @@
 import java.util.HashMap;
+import java.util.Map;
 
-public class HashTableExample {
+public class Main {
     public static void main(String[] args) {
-        // Использование HashMap как хэш-таблицы
-        HashMap<Integer, String> hashTable = new HashMap<>();
+        Map<String, Boolean> ht = new HashMap<>();
+        ht.put("Фонарь", true);
+        ht.put("Дверь", false);
+        ht.put("Окно", true);
 
-        // Добавление элементов
-        hashTable.put(1, "One");
-        hashTable.put(2, "Two");
-        hashTable.put(3, "Three");
-
-        // Поиск элемента
-        String value = hashTable.get(2);
-        System.out.println("Значение для ключа 2: " + value);
-
-        // Проверка наличия ключа
-        if (hashTable.containsKey(2)) {
-            System.out.println("Ключ 2 существует");
-        } else {
-            System.out.println("Ключ 2 не существует");
+        for (var entry : ht.entrySet()) {
+            System.out.println(entry.getKey() + ": " + (entry.getValue() ? "включен" : "выключен"));
         }
     }
+}
