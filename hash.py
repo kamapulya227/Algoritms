e78162b5b50e12125c0c1f369496bed4da16a1d5
kamapulya227 <kim.ase4ka@yandex.ru>
2025-10-02 16:01:33 +0300

    Update hash.py

--- hash.py
+++ hash.py
@@ -1,8 +1,8 @@
 hash = {}
 
-hash["Russia"] = "Moscow"
-hash["Germany"] = "Berlin"
-hash["Japan"] = "Tokyo"
+hash["person1"] = {"name": "John", "age": 30}
+hash["person2"] = {"name": "Alice", "age": 25}
+hash["person3"] = {"name": "Bob", "age": 35}
 
 for key, value in hash.items():
-    print(f"Capital of {key} is {value}")
+    print(f"{key}: {value['name']} is {value['age']} years old")
